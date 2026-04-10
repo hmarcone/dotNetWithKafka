35e0e319a2c79a369a821f58f6070ca497df80e9
hmarcone <hmarcone@gmail.com>
2026-04-10 18:25:07 -0300

    Adicionando o insert no banco sql do user e da mensagem(post)

--- Controllers/MensagemController.cs
+++ Controllers/MensagemController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ApiPostService.Context;
+using ApiPostService.Entities;
 using Confluent.Kafka;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 [ApiController]
 [Route("api/[controller]")]
@@ -7,16 +10,48 @@ public class MensagemController : ControllerBase
 {
 	private readonly IProducer<string, string> _producer;
 
-	public MensagemController(IProducer<string, string> producer)
+	private readonly PostServiceContext _context;
+
+
+	public MensagemController(IProducer<string, string> producer, PostServiceContext context)
 	{
 		_producer = producer;
+		_context = context;
+	}
+
+	[HttpGet]
+	public async Task<ActionResult<IEnumerable<Post>>> GetPost()
+	{
+		return await _context.Posts.Include(x => x.User).ToListAsync();
 	}
 
 	[HttpPost]
 	public async Task<IActionResult> Enviar([FromBody] string texto)
 	{
+
+		if (string.IsNullOrEmpty(texto))
+			return BadRequest();
+
+		var user = new User { Id = 2, Name = "hfreitas" };
+		var post = new Post
+		{
+			PostId = 2,
+			User = user,
+			Content = texto,
+			Title = $@"Mensagem do User {user.Id}"
+		};
+
+		_context.Posts.Add(post);
+
+		await _context.SaveChangesAsync();
+
+
 		var message = new Message<string, string> { Value = texto };
+
 		await _producer.ProduceAsync("meu-topico-teste", message);
-		return Ok($"Mensagem '{texto}' enviada com sucesso!");
+
+		return CreatedAtAction("GetPost", new { id = post.PostId }, post);
+
+		//return Ok($"Mensagem '{texto}' enviada com sucesso!");
 	}
 }
--- Program.cs
+++ Program.cs
@@ -1,9 +1,16 @@
+using ApiPostService.Context;
 using Confluent.Kafka;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration.GetConnectionString("Sqlite");
+builder.Services.AddDbContext<PostServiceContext>(options =>
+		options.UseSqlite(connectionString));
+
 
 // Configuração do Produtor (Singleton)
 var producerConfig = new ProducerConfig
@@ -18,6 +25,30 @@ builder.Services.AddSingleton<IProducer<string, string>>(
 builder.Services.AddHostedService<KafkaConsumerWorker>();
 
 var app = builder.Build();
+
+CreateDatabase(app);
+
 if (app.Environment.IsDevelopment()) { app.UseSwagger(); app.UseSwaggerUI(); }
+
+InjectDataBase(app.Services);
+
 app.MapControllers();
 app.Run();
+
+static void CreateDatabase(WebApplication app)
+{
+	var serviceScope = app.Services.CreateScope();
+	var dataContext = serviceScope.ServiceProvider.GetService<PostServiceContext>();
+	dataContext?.Database.EnsureCreated();
+}
+
+static void InjectDataBase(IServiceProvider serviceProvider)
+{
+		using (var scope = serviceProvider.CreateScope())
+		{
+			var scopedServiceProvider = scope.ServiceProvider;
+			var dbContext = scopedServiceProvider.
+							GetRequiredService<PostServiceContext>();
+	};
+
+}
