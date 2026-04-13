80a69e021c8ed7a209977ff54dd85a998ce0c744
hmarcone <hmarcone@gmail.com>
2026-04-13 15:16:32 -0300

    Configurando gitignore e removendo arquivos desnecessários e adicionando nova api

---
+++ ApiUser/Controllers/UserController.cs
@@ -0,0 +1,150 @@
+﻿using ApiPostService.Context;
+using ApiPostService.Entities;
+using Confluent.Kafka;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UserController : ControllerBase
+{
+	private readonly IProducer<string, string> _producer;
+
+	private readonly PostServiceContext _context;
+
+
+	public UserController(IProducer<string, string> producer, PostServiceContext context)
+	{
+		_producer = producer;
+		_context = context;
+	}
+
+	[HttpGet]
+	public async Task<ActionResult<IEnumerable<User>>> GetUser()
+	{
+		return await _context.Users.ToListAsync();
+	}
+
+	//[HttpGet]
+	//public async Task<ActionResult<IQueryable<Post>>> GetPost()
+	//{
+	//	return await _context.Posts.Include(x => x.User).ToListAsync();
+	//}
+
+	//[HttpPost]
+	//public async Task<IActionResult> Enviar([FromBody] string texto)
+	//{
+
+	//	if (string.IsNullOrEmpty(texto))
+	//		return BadRequest();
+
+	//	var user = new User { Id = 2, Name = "hfreitas" };
+	//	var post = new Post
+	//	{
+	//		PostId = 2,
+	//		User = user,
+	//		Content = texto,
+	//		Title = $@"Mensagem do User {user.Id}"
+	//	};
+
+	//	_context.Posts.Add(post);
+
+	//	await _context.SaveChangesAsync();
+
+
+	//	var message = new Message<string, string> { Value = texto };
+
+	//	await _producer.ProduceAsync("meu-topico-teste", message);
+
+	//	return CreatedAtAction("GetPost", new { id = post.PostId }, post);
+
+	//	//return Ok($"Mensagem '{texto}' enviada com sucesso!");
+	//}
+
+	
+	[HttpPut("{id}")]
+	public async Task<IActionResult> PutUser(int id, User user)
+	{
+		if (user == null || id <= 0)
+			return BadRequest();
+
+		_context.Entry(user).State = EntityState.Modified;
+
+		try
+		{
+			await _context.SaveChangesAsync();
+		}
+		catch (DbUpdateConcurrencyException)
+		{
+			// Verifique se o registro ainda existe no banco
+			var exists = await _context.Users.AnyAsync(e => e.Id == id);
+
+			if (!exists)
+			{
+				return NotFound("O registro foi excluído por outro usuário.");
+			}
+			else
+			{
+				throw; // Outro erro de concorrência (ex: RowVersion)
+			}
+		}
+
+
+		//await _context.SaveChangesAsync();
+
+		var integrationEventData = JsonSerializer.Serialize(new
+		{
+			id = user.Id,
+			newname = user.Name
+		});
+
+		PublishToMessageQueue(integrationEventData);
+
+		return NoContent();
+	}
+
+	[HttpPost]
+	public async Task<ActionResult<User>> PostUser(User user)
+	{
+		if (user is null)
+			return BadRequest();
+
+
+		// Verifique se o registro ainda existe no banco
+		var exists = await _context.Users.AnyAsync(e => e.Id == user.Id);
+
+		if (!exists)
+		{
+			_context.Users.Add(user);
+		}else{
+			user.Name += " alterado";
+
+			_context.Entry(user).State = EntityState.Modified;
+		}
+
+
+		await _context.SaveChangesAsync();
+
+		var integrationEventData = JsonSerializer.Serialize(new
+		{
+			id = user.Id,
+			name = user.Name
+		});
+
+		PublishToMessageQueue(integrationEventData);
+
+		return CreatedAtAction("GetUser", new { id = user.Id }, user);
+	}
+
+	private async void PublishToMessageQueue([FromBody] string texto)
+	{
+		//if (string.IsNullOrEmpty(texto))
+		//	return BadRequest();
+
+		var message = new Message<string, string> { Value = texto };
+
+		await _producer.ProduceAsync("meu-topico-teste", message);
+
+	}
+}
